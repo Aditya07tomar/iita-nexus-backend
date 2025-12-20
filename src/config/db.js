c46c98ffe573995393a0fe3bf910983434baff9d
Aditya07tomar <your-email@example.com>
2025-12-20 22:40:38 +0530

    Initial backend commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306, // Important for Aiven
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// Convert pool to use Promises so we can use async/await
-const promisePool = pool.promise();
-
-module.exports = promisePool;
+module.exports = pool.promise();
